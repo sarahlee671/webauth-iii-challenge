5a4bd2f4af74b416acbbc542edbcdb0896f4570c
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-12 11:33:26 -0700

    created a function to generate token and send to client when logged in and verified

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,7 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
                 res.status(200).json({
                     message: `Welcome ${user.username}!`,
                     token
@@ -37,3 +38,22 @@ router.post('/login', (req, res) => {
             res.status(500).json(error);
         });
 });
+
+function generateToken(user) {
+    const payload = {
+        subject: user.id, 
+        username: user.username,
+        department: ['student']
+    };
+
+    const options = {
+        expiresIn: '1d',
+
+    }
+
+
+    return jwt.sign(payload, secrets.jwtSecret, options)
+
+}
+
+module.exports = router;
