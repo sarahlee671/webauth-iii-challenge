b48095ba6a216cef847968297b313db6d3302af3
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-12 12:44:46 -0700

    added login component and added a link and route in my app.js folder for login

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Route, NavLink} from 'react-router-dom';
+import Login from './component/Login';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,19 +8,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavLink to="/login">Login</NavLink>
       </header>
+      <div>
+        <Route path = "/login" component={Login}> </Route>
+      </div>
+      
     </div>
   );
 }
