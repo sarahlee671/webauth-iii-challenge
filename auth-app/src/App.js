e835cb71a22a2cb6833cd083da14e6e8ad76a71d
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-12 14:29:17 -0700

    created login form and stored token in localstorage

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Route, NavLink} from 'react-router-dom';
 import Login from './component/Login';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <NavLink to="/login">Login</NavLink>
       </header>
       <div>
-        <Route path = "/login" component={Login}> </Route>
+        <Route path = "/login" component={Login}></Route>
       </div>
       
     </div>
--- auth-app/src/component/Login.js
+++ auth-app/src/component/Login.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
+    state = {
+        username: 'sarah',
+        password: 'password'
+    }
+
     render() {
-        return <h3>Login Component</h3>
+        return (
+            <div>
+                <h2>Login</h2>
+                <form onSubmit={this.submitForm}>
+                    <div>
+                        <label htmlFor="username" />
+                        <input
+                            id="username"
+                            onChange={this.handleChange}
+                            value={this.state.username}
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" />
+                        <input
+                            id="password"
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                            type="password"
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
+
+    handleChange = event => {
+        const {id, value} = event.target;
+
+        this.setState({ [id]: value });
+    };
+
+    submitForm = event => {
+        event.preventDefault();
+        const endpoint = 'http://localhost:5000/api/auth/login';
+
+        axios
+            .post(endpoint, this.state)
+            .then(res => {
+                localStorage.setItem('jwt', res.data.token)
+            })
+            .catch(err => {
+                console.error('Login Error', err)
+            })
     }
 }
 
