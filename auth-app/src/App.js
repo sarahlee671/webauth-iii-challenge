e8a8eb197af468cd632408a82940bd8ead5a18c8
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-12 19:46:42 -0700

    imported register component to app file and created navlink and path for register

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, NavLink, withRouter} from 'react-router-dom';
 import Login from './auth/Login';
 import Users from './users/Users';
+import Register from './auth/Register';
 
 import './App.css';
 
@@ -16,11 +17,13 @@ function App(props) {
       <header>
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/users">Users</NavLink>
+        <NavLink to="/register">Register</NavLink>
         <button type="button" onClick={logout}>Logout</button>
       </header>
       <div>
         <Route path = "/login" component={Login} />
         <Route path ="/users" component={Users} />
+        <Route path ="/register" component={Register} />
       </div>
     </div>
   );
